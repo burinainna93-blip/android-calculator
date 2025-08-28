7f468e8a7f4bcf3921a93164b5236c53fa0fb7bc
burinainna93-blip <burinainna93@gmail.com>
2025-08-28 18:41:12 +0300

    main.py

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class Calculator(GridLayout):
+    def __init__(self, **kwargs):
+        super(Calculator, self).__init__(**kwargs)
+        self.cols = 4
+
+        self.result = TextInput(multiline=False, readonly=True, halign="right", font_size=32)
+        self.add_widget(self.result)
+
+        buttons = [
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "C", "0", "=", "+"
+        ]
+
+        for button in buttons:
+            self.add_widget(Button(text=button, on_press=self.on_button_press))
+
+    def on_button_press(self, instance):
+        text = instance.text
+
+        if text == "C":
+            self.result.text = ""
+        elif text == "=":
+            try:
+                self.result.text = str(eval(self.result.text))
+            except Exception:
+                self.result.text = "Error"
+        else:
+            self.result.text += text
+
 
 class CalculatorApp(App):
     def build(self):
-        self.operators = ['+', '-', '*', '/']
-        self.last_was_operator = None
-        self.last_button = None
+        return Calculator()
 
-        main_layout = BoxLayout(orientation="vertical")
-        self.solution = TextInput(multiline=False, readonly=True, halign="right", font_size=55)
-        main_layout.add_widget(self.solution)
 
-        buttons = [
-            ['7', '8', '9', '/'],
-            ['4', '5', '6', '*'],
-            ['1', '2', '3', '-'],
-            ['.', '0', 'C', '+']
-        ]
-
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
-
-        equals_button = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5})
-        equals_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equals_button)
-
-        return main_layout
-
-    def on_button_press(self, instance):
-        current = self.solution.text
-        button_text = instance.text
-
-        if button_text == "C":
-            self.solution.text = ""
-        else:
-            if current and (self.last_was_operator and button_text in self.operators):
-                return
-            elif current == "" and button_text in self.operators:
-                return
-            else:
-                self.solution.text = current + button_text
-
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
-
-    def on_solution(self, instance):
-        text = self.solution.text
-        if text:
-            try:
-                self.solution.text = str(eval(self.solution.text))
-            except Exception:
-                self.solution.text = "Ошибка"
-
-if name == "__main__":
+if __name__ == "__main__":
     CalculatorApp().run()
